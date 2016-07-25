0f6db677f3990fe1bb86414348678ae51e7e3957
michelleheh <xiaoyu.he22@gmail.com>
2016-07-24 20:18:13 -0700

    Serve index.html

--- server/server.js
+++ server/server.js
@@ -3,4 +3,7 @@ const app = express();
 
 const port = 3000;
 
+// configure our server with all the middleware
+require('./config/middleware.js')(app, express);
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
