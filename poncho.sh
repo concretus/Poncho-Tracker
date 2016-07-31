c3c286f122ef6255f661713a95642b46601304ab
michelleheh <xiaoyu.he22@gmail.com>
2016-07-31 15:47:49 -0700

    fix typo

--- poncho.sh
+++ poncho.sh
@@ -1,4 +1,4 @@
 docker build -t fishfishher/poncho-tracker .
 echo "Starting server / client containter..."
-docker run -p 8080:8080 -d fishfisher/poncho-tracker
+docker run -p 8080:8080 -d fishfishher/poncho-tracker
 echo "Running server / client containter..."
