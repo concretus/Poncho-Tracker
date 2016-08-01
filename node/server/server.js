db6ebafac0c78dcf061869e6e15c8a84a3c90323
michelleheh <xiaoyu.he22@gmail.com>
2016-08-01 16:04:10 -0700

    Create table in database

--- node/server/server.js
+++ node/server/server.js
@@ -3,11 +3,11 @@ const app = express();
 const pg = require('pg');
 
 const port = 8080;
-// const conString = "postgres://"+process.env.POSTGRES_USER+":"+process.env.POSTGRES_PASSWORD+"@postgres:5432/"+process.env.POSTGRES_DATABASE;
+// const conString = "postgres://"+process.env.POSTGRES_USER+":"+process.env.POSTGRES_PASSWORD+"@postgres:5432/"+process.env.POSTGRES_DB;
 
 console.log('process.env.POSTGRES_PASSWORD: ', process.env.POSTGRES_PASSWORD);
 console.log('process.env.POSTGRES_USER: ', process.env.POSTGRES_USER);
-console.log('process.env.POSTGRES_DATABASE: ', process.env.POSTGRES_DATABASE);
+console.log('process.env.POSTGRES_DB: ', process.env.POSTGRES_DB);
 // configure our server with all the middleware
 require('./config/middleware.js')(app, express);
 
@@ -32,28 +32,58 @@ app.get('/testpg', (req, res) => {
 
 app.get('/testbookshelf', (req, res) => {
 
-  // var knex = require('knex')({
-  //   client: 'pg',
-  //   connection: {
-  //     host     : 'postgres',
-  //     user     : process.env.POSTGRES_USER,
-  //     password : process.env.POSTGRES_PASSWORD,
-  //     database : process.env.POSTGRES_DATABASE,
-  //     charset  : 'utf8'
-  //   }
+  var knex = require('knex')({
+    client: 'pg',
+    connection: {
+      host     : process.env.POSTGRES_HOST,
+      user     : process.env.POSTGRES_USER,
+      password : process.env.POSTGRES_PASSWORD,
+      database : process.env.POSTGRES_DB,
+      charset  : 'utf8'
+    }
+  });
+
+  var bookshelf = require('bookshelf')(knex);
+
+  // knex.schema.createTableIfNotExists('users', function (table) {
+  //   table.increments();
+  //   table.string('name');
+  //   table.timestamps();
   // });
 
-  // var bookshelf = require('bookshelf')(knex);
+  return knex.schema.createTableIfNotExists('users', function(t) {
+    t.increments('id').primary();
+    t.string('first_name', 100);
+    t.string('last_name', 100);
+    t.text('bio');
+  }).then(function(){
+    res.send('testing bookshelf');
+  });
+
+  // knex.schema.hasTable('users').then(function(exists) {
+  //   if (!exists) {
+  //     return knex.schema.createTable('users', function(t) {
+  //       t.increments('id').primary();
+  //       t.string('first_name', 100);
+  //       t.string('last_name', 100);
+  //       t.text('bio');
+  //     });
+  //   } else 
+  // });
 
   // var User = bookshelf.Model.extend({
-  //   tableName: 'users'
+  //   tableName: 'customer'
   // });
 
-  // new User().fetchAll()
-  //   .then(function(collection){
-  //     console.log(collection);
-  //     res.send('Hello bookshelf');
-  //   });
+  // User.collection().fetch().then(function (collection) {
+  //   console.log(collection);
+    // res.send('testing bookshelf');
+  // });
+
+  
+  
+
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
