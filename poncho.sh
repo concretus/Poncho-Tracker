2123e1e9bdc0db37ecaf5a23a626f4fd16e43b1f
michelleheh <xiaoyu.he22@gmail.com>
2016-08-01 13:18:50 -0700

    reconfigure folders

--- poncho.sh
+++
@@ -1,4 +0,0 @@
-docker build -t fishfishher/poncho-tracker .
-echo "Starting server / client containter..."
-docker run -p 8080:8080 -d fishfishher/poncho-tracker
-echo "Running server / client containter..."
