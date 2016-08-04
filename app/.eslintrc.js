7aaf892065957ba7388bbca87ff8dae8415f50b4
michelleheh <xiaoyu.he22@gmail.com>
2016-08-04 12:29:04 -0700

    Add mocha test

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "es6": true,
     "browser": true,
-    "node": true
+    "node": true,
+    "mocha": true
   },
   "extends": "eslint:recommended",
   "parserOptions": {
