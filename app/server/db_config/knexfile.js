62286d2e2e90e0be94d82337a4f68c2982db1c3e
michelleheh <xiaoyu.he22@gmail.com>
2016-08-05 10:01:30 -0700

    Pass dummy db test

--- app/server/db_config/knexfile.js
+++ app/server/db_config/knexfile.js
@@ -18,14 +18,13 @@ module.exports = {
   },
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/poncho_db_dev',
-    // connection: {
-      // host     : process.env.POSTGRES_HOST,
-      // user     : process.env.POSTGRES_USER,
-      // password : process.env.POSTGRES_PASSWORD,
-      // database : process.env.POSTGRES_DB,
-      // charset  : 'utf8'
-    // },
+    connection: {
+      host     : 'localhost',
+      user     : 'postgres',
+      password : '',
+      database : 'poncho_db_test',
+      charset  : 'utf8'
+    },
     migrations: {
       directory: __dirname +'/migrations'
     },
--- app/server/test/api.spec.js
+++ app/server/test/api.spec.js
@@ -7,26 +7,18 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
-describe('API Routes', () =>{
+describe('API Routes', () => {
   describe('GET /api/v1/pageview', function() {
     it('should return all pageview', function(done) {
       chai.request(server)
       .get('/api/v1/pageview')
       .end(function(err, res) {
         res.should.have.status(200);
-        res.should.be.json; // jshint ignore:line
+        res.should.be.json;
         res.body.should.be.a('array');
-        res.body.length.should.equal(4);
-        res.body[0].should.have.property('name');
-        res.body[0].name.should.equal('Suits');
-        res.body[0].should.have.property('channel');
-        res.body[0].channel.should.equal('USA Network');
-        res.body[0].should.have.property('genre');
-        res.body[0].genre.should.equal('Drama');
-        res.body[0].should.have.property('rating');
-        res.body[0].rating.should.equal(3);
-        res.body[0].should.have.property('explicit');
-        res.body[0].explicit.should.equal(false);
+        res.body.length.should.equal(2);
+        res.body[0].should.have.property('uid');
+        res.body[0].uid.should.equal(1);
         done();
       });
     });
