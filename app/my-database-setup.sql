d22b077d83f3ab59346923253ecf458f985b1581
michelleheh <xiaoyu.he22@gmail.com>
2016-08-05 13:51:31 -0700

    Update test database

--- app/my-database-setup.sql
+++ app/my-database-setup.sql
@@ -3,7 +3,7 @@
 -- GRANT ALL PRIVILEGES ON DATABASE docker_db TO docker;
 CREATE DATABASE poncho_db_test;
 
-\c poncho_db_dev
+\c poncho_db_test
 CREATE TABLE pageview(
   uid INT PRIMARY KEY NOT NULL
 );
