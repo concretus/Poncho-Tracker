588db508e03d4f53c9f24d9501782721c7179f21
michelleheh <xiaoyu.he22@gmail.com>
2016-08-07 22:15:10 -0700

    Update postgres initial database

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,8 +1,25 @@
 -- 
 CREATE USER docker;
-GRANT ALL PRIVILEGES ON DATABASE docker_db TO docker;
+GRANT ALL PRIVILEGES ON DATABASE poncho_db TO docker;
 
-\c docker_db;
-CREATE TABLE test(
-  ID INT PRIMARY KEY NOT NULL
+\c poncho_db
+
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  created_at TIMESTAMP default now()
+);
+
+CREATE TABLE rfis(
+  id SERIAL PRIMARY KEY,
+  rfi_number DECIMAL NOT NULL,
+  date_created TIMESTAMP NOT NULL,
+  date_due TIMESTAMP NOT NULL,
+  title VARCHAR(200) NOT NULL,
+  question TEXT NOT NULL,
+  related_rfi INT references rfis(id),
+  created_by INT NOT NULL references users(id)
 );
